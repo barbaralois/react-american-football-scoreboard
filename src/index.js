62cb9d537490892744fc0cb75fc19bef4aa56357
Barbara Moore <barbaraloismoore@gmail.com>
2020-05-11 17:37:37 -0400

    completed MVP and began on stretch

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppStretch from './AppStretch';
 
+//To see the original assignment
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//To see implementation with stretch goals
+// ReactDOM.render(<AppStretch />, document.getElementById('root'));
